94c0bcedb319939bb153526472cc3dc1d38528d9
Martin Høite <martin@hoite.dev>
2024-08-01 01:37:16 +0200

    Initial slug setup with favicons

---
+++ app/pages/[...slug].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: 'favicon.svg'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: 'favicon.png'
+    }
+  ]
+});
+</script>
+<template>
+  <!-- <section></section> -->
+  <ViewsKitchenSink />
+</template>
+<style lang="postcss" scoped></style>
