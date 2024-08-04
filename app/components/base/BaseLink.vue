e9f3eda5bcc0e84f9e0c41358079fb112b61b28b
Martin Høite <martin@hoite.dev>
2024-08-04 04:23:17 +0200

    Initial setup for BaseLink component

--- app/components/base/BaseLink.vue
+++ app/components/base/BaseLink.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { SimplifiedUmbracoLink } from '~/types/umbraco';
+type Props = {
+  to?: SimplifiedUmbracoLink;
+};
+
+defineProps<Props>();
+</script>
 <template>
-  <section></section>
+  <NuxtLink
+    v-if="to?.url"
+    :to="to.url"
+  >
+    <slot></slot>
+  </NuxtLink>
 </template>
 <style lang="postcss" scoped></style>
