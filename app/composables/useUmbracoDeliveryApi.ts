31a7d9c05fab4fce96647c0636421b68e7464a94
Martin Høite <martin@hoite.dev>
2024-08-11 21:42:55 +0200

    Implemented & typed separate functions for getting UmbracoContent with all filters, sorts etc.

--- app/composables/useUmbracoDeliveryApi.ts
+++ app/composables/useUmbracoDeliveryApi.ts
@@ -1,25 +1,57 @@
-import type { Locale } from '~/types';
-import type { UmbracoSiteSettingsResponse } from '~/types/umbraco';
+import type { Locale } from 'types';
+import type { UmbracoSiteSettingsResponse } from 'types/umbraco';
 import type {
-  UmbracoContentHeaders,
+  UmbracoAlias,
+  UmbracoContentResponse,
   UmbracoDeliveryApiResponse,
+  UmbracoNodePath,
   UmbracoPageResponse
-} from '~/types/umbracoDeliveryApi';
+} from 'types/umbracoDeliveryApi';
 
 export default function () {
-  const getUmbracoContentByRoute = <PropertiesType = UmbracoPageResponse>(path: string) => {
-    // TODO Extract startItem, culture and path handling for reuse.
+  const getCurrentStartItem = (): string => {
     const {
-      public: { localDevelopmentSubdomain, fallbackLocale }
+      public: { localDevelopmentSubdomain }
     } = useRuntimeConfig();
-    const startItem = getSubdomain(useRequestHeaders().host) || localDevelopmentSubdomain;
-    const culture = getLocaleFromPath(path) || fallbackLocale;
-    const pathWithoutLocale = removeLocaleFromPath(path);
+    return getSubdomain(useRequestHeaders().host) || localDevelopmentSubdomain;
+  };
+
+  const setDefaultHeaders = (path: string = '', startItem: string = '') => {
+    const {
+      public: { fallbackLocale }
+    } = useRuntimeConfig();
+    const culture = getLocaleFromPath(path) || (fallbackLocale as Locale);
 
     const headers: HeadersInit = {
       'Accept-Language': culture,
-      'Start-Item': startItem
+      'Start-Item': startItem !== '' ? startItem : getCurrentStartItem()
     };
+
+    return headers;
+  };
+
+  /**
+   *
+   * @param parameters
+   * @param customStartItem if not set, will default to the current subdomain as start item
+   * @returns
+   */
+  const getUmbracoContent = <ItemTypes = unknown>(
+    parameters: UmbracoContentParameters,
+    customStartItem: string = ''
+  ) => {
+    const headers = setDefaultHeaders(customStartItem);
+
+    return UseFetch<UmbracoContentResponse<ItemTypes>>('/umbraco/delivery/api/v2/content', {
+      keepalive: true,
+      params: parameters,
+      headers
+    });
+  };
+
+  const getUmbracoContentByRoute = <PropertiesType = UmbracoPageResponse>(path: string) => {
+    const pathWithoutLocale = removeLocaleFromPath(path);
+    const headers = setDefaultHeaders(path);
 
     return UseFetch<UmbracoDeliveryApiResponse<PropertiesType>>(
       `/umbraco/delivery/api/v2/content/item${pathWithoutLocale}`,
@@ -34,29 +66,27 @@ export default function () {
     return getUmbracoContentByRoute<PropertiesType>(`/${locale}/settings`);
   };
 
-  // TODO: Create separate function for getting "list" content, and use that here instead of hardcoding the parameters
-  const getUmbracoNavigationItems = (locale: Locale) => {
-    // TODO: Extract startItem, culture and path handling for reuse.
-    const {
-      public: { localDevelopmentSubdomain }
-    } = useRuntimeConfig();
-    const startItem = getSubdomain(useRequestHeaders().host) || localDevelopmentSubdomain;
+  const getUmbracoNavigationItems = () => {
+    // fetch=descendants%3A%2F&filter=contentType%3A%21siteSettings&fields=properties%5BincludeInNavigation%2CincludeChildrenInNavigation%5D`,
+    // fetch=descendants:/&filter=contentType:!siteSettings&fields=properties[includeInNavigation,includeChildrenInNavigation]`,
 
-    const headers: HeadersInit = {
-      'Accept-Language': locale,
-      'Start-Item': startItem
-    };
+    return getUmbracoContent<UmbracoNavigationItemProperties>({
+      fetch: `descendants:${'/' as UmbracoNodePath}`,
+      filter: `contentType:${'!siteSettings' as UmbracoAlias}`,
+      fields: `properties${'[includeInNavigation, includeChildrenInNavigation]' as UmbracoPropertyAliasArray}`
+    });
 
-    return UseFetch<UmbracoNavigationItemsResponse>(
-      `/umbraco/delivery/api/v2/content?fetch=descendants%3A%2F&filter=contentType%3A%21siteSettings&fields=properties%5BincludeInNavigation%2CincludeChildrenInNavigation%5D`,
-      {
-        keepalive: true,
-        headers
-      }
-    );
+    // return UseFetch<UmbracoContentResponse<UmbracoNavigationItemProperties>>(
+    //   `/umbraco/delivery/api/v2/content?
+    //   {
+    //     keepalive: true,
+    //     headers
+    //   }
+    // );
   };
 
   return {
+    getUmbracoContent,
     getUmbracoContentByRoute,
     getUmbracoSiteSettings,
     getUmbracoNavigationItems
--- app/types/umbracoDeliveryApi.ts
+++ app/types/umbracoDeliveryApi.ts
@@ -1,4 +1,5 @@
-import type { Locale } from '.';
+import type { Locale } from 'types';
+import type { ISODateOnlyString } from 'types/utilityTypes';
 
 export type UmbracoContentTypes = 'website' | 'contentPage';
 
@@ -32,7 +33,96 @@ export type UmbracoPageResponse = ExpandRecursively<UmbracoSeoAndMetaPageSetting
     blocks?: unknown;
   };
 
-export type UmbracoNavigationItemsResponse = {
+export type UmbracoContentResponse<ItemType> = {
   total: number;
-  items: UmbracoDeliveryApiResponse<UmbracoNavigationItemProperties>[];
+  items: UmbracoDeliveryApiResponse<ItemType>[];
+};
+
+/**
+ * Represents the ID of a node in Umbraco.
+ *
+ * Expects a string in the form of a GUID from the node in Umbraco.
+ */
+export type UmbracoNodeId = string & { readonly __umbracoNodeIdBrand: unique symbol };
+
+/**
+ * Represents the path to a node in Umbraco.
+ *
+ * Expects a string of a unique path to a specific node in Umbraco.
+ */
+export type UmbracoNodePath = string & { readonly __umbracoNodePathBrand: unique symbol };
+
+/**
+ * Represents the alias of a node in Umbraco.
+ *
+ * Expects a string of a unique alias from a node in Umbraco.
+ */
+export type UmbracoAlias = string & { readonly __umbracoAliasBrand: unique symbol };
+
+/**
+ * Represents the name of a node in Umbraco.
+ *
+ * Expects the name of a node in Umbraco.
+ */
+export type UmbracoNodeName = string & { readonly __umbracoNodeNameBrand: unique symbol };
+
+export type SortDirection = 'asc' | 'desc';
+
+export type AncestorsParameterPattern = `ancestors:${UmbracoNodeId | UmbracoNodePath}`;
+export type ChildrenParameterPattern = `children:${UmbracoNodeId | UmbracoNodePath}`;
+export type DescendantsParameterPattern = `descendants:${UmbracoNodeId | UmbracoNodePath}`;
+
+export type FilterByContentPattern = `contentType:${UmbracoAlias}`;
+export type FilterByNodeNamePattern = `name:${UmbracoNodeName}`;
+export type FilterByCreationDateLessThanPattern = `createDate<${ISODateOnlyString}`;
+export type FilterByUpdateDateLessThanOrEqualPattern = `updateDate>:${ISODateOnlyString}`;
+
+export type SortByCreateDatePattern = `createDate:${SortDirection}`;
+export type SortByUpdateDatePattern = `updateDate:${SortDirection}`;
+export type SortByLevelPattern = `level:${SortDirection}`;
+export type SortByNamePattern = `name:${SortDirection}`;
+export type SortBySortOrderPattern = `sortOrder:${SortDirection}`;
+
+/**
+ * Represents an array of one or more node property aliases (can be nested) in Umbraco.
+ * @example
+ * [alias1]
+ * [alias1,alias2]
+ * [alias1,alias2]
+ * [alias1,properties[nestedAlias1]]
+ * [alias1,properties[nestedAlias1,nestedAlias2]]
+ * [alias1,alias2,alias3properties[nestedAlias1,nestedAlias2]] // Gets the properties of alias1, alias2, and the specific nestedAliases from alias3.
+ */
+export type UmbracoPropertyAliasArray = string & { readonly __umbracoPropertyAliasArrayBrand: unique symbol };
+
+export type UmbracoPropertyTypes = 'properties[$all]' | `properties${UmbracoPropertyAliasArray}`;
+
+type FilterPattern =
+  | FilterByContentPattern
+  | FilterByNodeNamePattern
+  | FilterByCreationDateLessThanPattern
+  | FilterByUpdateDateLessThanOrEqualPattern;
+
+type SortPattern =
+  | SortByCreateDatePattern
+  | SortByUpdateDatePattern
+  | SortByLevelPattern
+  | SortByNamePattern
+  | SortBySortOrderPattern;
+
+export type UmbracoContentParameters = {
+  fetch: 'fetch' | AncestorsParameterPattern | ChildrenParameterPattern | DescendantsParameterPattern;
+  filter?: FilterPattern | FilterPattern[];
+  sort?: SortPattern | SortPattern[];
+  skip?: number;
+  take?: number;
+  expand?: UmbracoPropertyTypes;
+  fields: UmbracoPropertyTypes;
+};
+
+export type UmbracoContentHeaders = HeadersInit & {
+  'Accept-Language': Locale;
+  'Api-Key'?: string;
+  Preview?: boolean;
+  'Start-Item': UmbracoNodeId | UmbracoNodePath;
 };
