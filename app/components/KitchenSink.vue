80e0f03b14cac0a78a54a83e67c23b2509edae5c
Martin Høite <martin@hoite.dev>
2024-08-26 01:49:15 +0200

    Added additional debug to KitchenSink for BaseGridContainer (TODO: 2-3 actually ending at 4 is somewhat confusing...)

--- app/components/KitchenSink.vue
+++ app/components/KitchenSink.vue
@@ -17,4 +17,23 @@
       quam possimus quo quia beatae quae.
     </p>
   </BaseGridContainer>
+  <BaseGridContainer
+    :mobile-start-column="2"
+    :mobile-end-column="3"
+  >
+    <h2>This container takes starts at 2, ends at 3</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  </BaseGridContainer>
+
+  <BaseGridContainer>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quibusdam deleniti debitis eveniet molestias placeat
+      sequi reprehenderit praesentium! Nihil laboriosam veniam, corporis pariatur voluptatem totam exercitationem porro
+      similique consequatur laborum. Facilis voluptatum iusto tenetur ipsa doloremque similique porro tempora
+      voluptatibus quo vel, et ullam nam esse modi doloribus reiciendis saepe eligendi deleniti dolore ad eos sed
+      accusantium praesentium est? Laborum? Molestias dolorum assumenda itaque accusamus maiores sunt asperiores
+      sapiente, vero nobis est inventore ut sed, exercitationem doloremque aliquid quis fugit quam! Aliquid modi minima,
+      quam possimus quo quia beatae quae.
+    </p>
+  </BaseGridContainer>
 </template>
