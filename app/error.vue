cc0658051389ab13fb1c8d874bac3c7e9ce5c8ca
Martin Høite <martin@hoite.dev>
2025-07-15 15:47:17 +0200

    Added simple error page setup

---
+++ app/error.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+
+const props = defineProps<{
+  error: NuxtError;
+}>();
+
+const { settings } = useSettings();
+
+useHead({
+  title: props.error?.statusCode.toString(),
+  titleTemplate(title: string | undefined) {
+    return title ? `${title} | ${settings.metaTitleExtension}` : settings.metaTitleExtension;
+  }
+});
+</script>
+
+<template>
+  <div>
+    <NuxtLayout>
+      <div class="container">
+        <div class="align-center flex flex-col items-center gap-8 py-28 text-center">
+          <div>
+            <h1 class="mb-4 text-[60px] lg:text-[96px]">
+              {{ error?.statusCode }}
+            </h1>
+          </div>
+        </div>
+      </div>
+    </NuxtLayout>
+  </div>
+</template>
